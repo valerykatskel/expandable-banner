aa2e23a3b083ef035aeab8bcddceaab85cef6160
handlar <ne.vse.srazu@gmail.com>
2019-08-06 03:44:16 +0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="tb-vision">
-    <div 
-      class="tb-vision-wrapper"
-    >
-      <div class="tb-vision-part tb-vision--small">
-        <banner-part 
-          frameSrc="https://vignette.wikia.nocookie.net/openarena/images/c/cc/Oa_banner_300x80_rocket.png/revision/latest?cb=20100607072258" 
-          frameWidth="300"
-          frameHeight="80"
-        />
-      </div>
-      <div class="tb-vision--big">
-        <banner-part 
-          frameSrc="https://cdn.thefabricator.com/mediakits/images/square-banner300x300.jpg" 
-          frameWidth="300"
-          frameHeight="300"
-        />
-      </div>
-      <div class="tb-vision-button"></div>
+    <div class="settings">
+      <span>yPos: {{yPos}}</span>
+      <span>bannerHeight: {{bannerHeight}}</span>
+      <span>bannerDragActive: {{bannerDragActive}}</span>
+      <span>bannerSmallOpacity: {{bannerSmallOpacity}}</span>
     </div>
+    <div class="tb-vision-holder">
+      <div class="tb-vision-wrapper" :style="inlineHeight">
+        <div class="tb-vision-part tb-vision--small" :style="inlineSmallOpacity">
+          <img src="./assets/banner-small.png" alt="">
+        </div>
+        <div class="tb-vision-part tb-vision--big">
+          <banner-part 
+            frameSrc="https://cdn.thefabricator.com/mediakits/images/square-banner300x300.jpg" 
+            frameWidth="300"
+            frameHeight="300"
+            frameId="bannerFrameBig"
+          />
+        </div>
+      </div>
+      <div
+        class="tb-vision-button" 
+        v-touch:start="onButtonDown"
+        v-touch:moving="onMoveBanner"
+      ></div>
+    </div> 
   </div>
 </template>
 
@@ -27,20 +34,91 @@ import BannerPart from './components/BannerPart.vue'
 
 export default {
   name: 'expandableBanner',
+
   components: {
     BannerPart
   },
+
   data () {
     return {
-      bannerHeight: 80
+      bannerHeight: 80,
+      bannerDragActive: false,
+      yPos: 0,
+      bannerSmallOpacity: 1,
     }
+  },
+
+  methods: {
+    onButtonDown (event) {
+      console.log('button pressed (tapped)');
+      this.bannerDragActive = true;
+      this.yPos = event.changedTouches[0].clientY;
+    },
+    
+    inlineSmallopacity () {
+      return `opacity: ${this.bannerSmallOpacity}`;
+    },
+
+    onMoveBanner (event) {
+      if (this.bannerDragActive) {
+        //console.log('banner is moving');
+        let diff = event.changedTouches[0].clientY - this.yPos;
+        this.yPos = event.changedTouches[0].clientY;
+        this.bannerHeight += diff;
+        let opacityDiff = (diff*(100/220))/100;
+        this.bannerSmallOpacity -= opacityDiff;
+        if (this.bannerSmallOpacity < 0) this.bannerSmallOpacity = 0;
+        if (this.bannerSmallOpacity > 1) this.bannerSmallOpacity = 1;
+        
+        if (this.bannerHeight < 80) this.bannerHeight = 80;
+        if (this.bannerHeight > 300) this.bannerHeight = 300;
+        
+        //document.querySelector('.tb-vision-wrapper').style.height = this.bannerHeight;
+      }
+    },
+    
+    onButtonUp () {
+      console.log('button unpressed');
+      this.bannerDragActive = false;
+      this.yPos = 0;
+    },
+  },
+
+  watch: {
+    bannerHeight (newValue, oldValue) {
+      if (newValue === 80) {
+        this.yPos = 0;
+        this.bannerDragActive = false;
+      }
+    }
+  },
+
+  computed: {
+    inlineHeight () {
+      return `height: ${this.bannerHeight}px`;
+    },
+    
+
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  .settings {
+    border: 1px solid red;
+    position: absolute;
+    span {
+      display: block;
+    }
+  }
   :root {
     --var-b-height: 80px;
+  }
+  .tb-vision-holder {
+    position: relative;
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
   }
   .tb-vision-button {
     cursor: pointer;
@@ -48,15 +126,38 @@ export default {
     width: 16px;
     height: 16px;
     border-radius: 50%;
-  }
-  .tb-vision-wrapper {
-    width: 300px;
     margin-left: auto;
     margin-right: auto;
+    transition: box-shadow 400ms;
+    &:hover {
+      box-shadow: 0px 0px 8px 8px #fa94946e;
+    }
+  }
+  .tb-vision-wrapper {
     overflow: hidden;
     height: 80px;
-  }
-  .tb-vision-part {
-    cursor: pointer;
+    position: relative;
+    border: 1px solid black;
+    
+    .tb-vision-part {
+      cursor: pointer;
+      background-color: #000;
+
+      &.tb-vision--small {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        img {
+          height: 100%;
+          width: 100%;
+          opacity: 1;
+        }
+      }
+      &.tb-vision--big {
+        opacity: 0;
+      }
+    }
   }
 </style>
--- src/components/BannerPart.vue
+++ src/components/BannerPart.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>  
+  <div class="resp-container" @message="getMessage">  
     <iframe 
+      :id="this.frameId"
       :src="this.frameSrc" 
       :width="this.frameWidth" 
       :height="this.frameHeight" 
-      frameborder="0"></iframe>  
+      frameborder="0"
+      @load="iframeStyles"
+      ref="iframeContent"
+    ></iframe>  
   </div>
 </template>
 
@@ -12,13 +16,45 @@
 export default {
   name: 'BannerPart',
   props: {
+    frameId: String,
     frameSrc: String,
     frameWidth: String,
     frameHeight: String
-  }
+  },
+  
+  methods: {
+    getMessage () {
+      console.log('there is a message')
+    },
+
+    iframeStyles () {
+      //console.log(parent)
+      
+      let frame = this.$refs.iframeContent;
+      console.log(frame)
+      // let css = document.createElement('style')
+      // css.type = 'text/css'
+
+      // let styles = `
+      //   body {
+      //     background-color: lime;
+      //     color: pink;
+      //   }`
+
+      //this.frame.postMessage('OK', '*')
+      // css.appendChild(document.createTextNode(styles))
+      // let head = frame.document.getElementsByTagName("head")[0];
+      //console.log(frame)
+      //head.appendChild(css)
+    },
+  },
+
 }
 </script>
 
 <style scoped>
-
+  .resp-container {
+    position: relative;
+    overflow: hidden;
+  }
 </style>
